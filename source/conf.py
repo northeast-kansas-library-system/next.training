b9a5cc06ceb872e421b4940cba92aa1194328cad
George <gwilliams@nekls.org>
2020-08-10 14:12:12 -0500

    Update thematic elements

--- source/conf.py
+++ source/conf.py
@@ -81,6 +81,8 @@ pygments_style = 'sphinx'
 
 html_theme = "sphinx_rtd_theme"
 
+html_logo = 'next.grey.small.png'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -92,6 +94,12 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    'css_files': [
+        '_static/override.css',  # overrides for wide tables in RTD theme
+        ],
+    }
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
